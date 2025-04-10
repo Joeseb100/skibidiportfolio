72af4f1e9bb820d4662fdbad965a44fa27cd157f
joeseb100 <joeseb100@github.com>
2025-04-10 21:45:39 +0530

    kanye loves u all

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Set base path to relative path for GitHub Pages deployment
 })
